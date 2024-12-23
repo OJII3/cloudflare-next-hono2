0d59d84e2e91424060689ce6614c772be3cb9b8d
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-12-23 16:02:56 +0900

    work

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,17 @@
+import { getRequestContext } from "@cloudflare/next-on-pages";
+
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+const route = app.get("/ping", async (c) => {
+	const { env } = getRequestContext();
+	return c.text(env.DISCORD_PUBLIC_KEY, 200);
+});
+
+type AppType = typeof route;
+const GET = handle(app);
+export { GET, type AppType };
